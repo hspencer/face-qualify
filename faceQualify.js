e29ecae973742d8e230fe5e430f36170ab26aa70
Herbert Spencer <hspencer@ead.cl>
2018-09-23 22:50:50 -0300

    better tolerance

--- faceQualify.js
+++ faceQualify.js
@@ -90,10 +90,10 @@ function touchEnded() {
 }
 
 function overSlider(){
-	if (mouseX > m && 
-		  mouseX < width - m &&
-		  mouseY > canvas.height - 60+m &&
-		  mouseY < canvas.height - 60+m + 2*m){
+	if (mouseX > 0 && 
+		  mouseX < width &&
+		  mouseY > canvas.height - 60 &&
+		  mouseY < canvas.height - 60+3*m){
 		return true;
 	}else{
 		return false;
@@ -136,7 +136,7 @@ function draw() {
 	}
 
 	face.drawFace();
-	drawSlider(canvas.height - 60);
+	drawSlider(canvas.height - 4 * m);
 	drawVal();
 	debug();
 }
